c4787726c1c76ae0b162041a219b2e3db5fb48e5
Viktoria Kondrashova <kavidmi@gmail.com>
2025-12-12 15:34:59 +0300

    feat: add avatar

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import { Logo } from "@shared/ui/Logo";
 import { Button } from "@shared/ui/button";
 import { ReactComponent as WalletIcon } from "@assets/icons/wallet.svg";
+import { AppAvatar } from "@widgets/Avatar/ui/Avatar";
 import "../Header.scss";
 
 export const Header = () => {
@@ -9,10 +10,13 @@ export const Header = () => {
   return (
     <header className="header">
       <Logo />
-      <div>
+      <div className="header-wrapper">
         <div className="header-balance">
           <div className="current-balance">{balance}</div>
           <Button icon={WalletIcon}>Пополнить</Button>
+        </div>
+        <div className="avatar-container">
+          <AppAvatar />
         </div>
       </div>
     </header>
