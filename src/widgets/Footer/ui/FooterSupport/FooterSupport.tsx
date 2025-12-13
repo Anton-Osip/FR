1b6d114ffd19653d7ecdf215f12df32a05a3fa8a
Viktoria Kondrashova <kavidmi@gmail.com>
2025-12-13 16:27:16 +0300

    feat: add footer support component

---
+++ src/widgets/Footer/ui/FooterSupport/FooterSupport.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@shared/ui";
+import { ReactComponent as SupportIcon } from "@assets/icons/support.svg";
+import type { FC } from "react";
+import "./FooterSupport.scss";
+
+interface Props {
+  className?: string;
+}
+
+export const FooterSupport: FC<Props> = ({className}) => {
+  return (
+    <div className={`footer-support ${className ?? ""}`}>
+      <h3>
+        Тех. поддержка <span className="footer-support-time">24/7</span>
+      </h3>
+      <Button size="s" icon={SupportIcon} variant="secondary">
+        Написать
+      </Button>
+    </div>
+  );
+};
