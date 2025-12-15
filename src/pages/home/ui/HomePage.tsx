fc3e699dc49ba5c564f5dd7c21ccbe62181c1039
Viktoria Kondrashova <kavidmi@gmail.com>
2025-12-15 17:12:02 +0300

    feat: add hero section

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,20 +1,24 @@
 import { Carousel } from "@widgets/Carousel";
 import { carouselData } from "../constants/constants";
+import { HeroSection } from "@widgets/HeroSection";
 import styles from "../HomePage.module.scss";
 
 export const HomePage = () => {
   return (
-    <div className={styles["main-carousel-section"]}>
-      {carouselData.map((item) => {
-        return (
-          <Carousel
-            key={item.title}
-            icon={item.icon}
-            title={item.title}
-            items={item.items}
-          />
-        );
-      })}
-    </div>
+    <>
+      <HeroSection />
+      <div className={styles["main-carousel-section"]}>
+        {carouselData.map((item) => {
+          return (
+            <Carousel
+              key={item.title}
+              icon={item.icon}
+              title={item.title}
+              items={item.items}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
