a9ed5459692f12342b83ef5ddc2d86351743640e
stas <kepamuk@yandex.ru>
2025-12-15 19:53:02 +0300

    fixed

--- src/widgets/Footer/ui/FooterSupport/FooterSupport.tsx
+++ src/widgets/Footer/ui/FooterSupport/FooterSupport.tsx
@@ -1,24 +1,24 @@
-import { Button } from "@shared/ui/button";
-import { ReactComponent as SupportIcon } from "@assets/icons/support.svg";
-import type { FC } from "react";
+import {Button} from "@shared/ui/button";
+import type {FC} from "react";
 import styles from "./FooterSupport.module.scss";
+import {SupportIcon} from "@shared/ui/icons";
 
 
 interface Props {
-  className?: string;
+    className?: string;
 }
 
 export const FooterSupport: FC<Props> = ({className}) => {
-  return (
-    <div className={`${styles["footer-support"]} ${
-        className ? styles[className] : ""
-      }`}>
-      <h3>
-        Тех. поддержка <span className={styles["footer-support-time"]}>24/7</span>
-      </h3>
-      <Button size="s" icon={SupportIcon} variant="secondary">
-        Написать
-      </Button>
-    </div>
-  );
+    return (
+        <div className={`${styles["footer-support"]} ${
+            className ? styles[className] : ""
+        }`}>
+            <h3>
+                Тех. поддержка <span className={styles["footer-support-time"]}>24/7</span>
+            </h3>
+            <Button size="s" icon={<SupportIcon/>} variant="secondary">
+                Написать
+            </Button>
+        </div>
+    );
 };
