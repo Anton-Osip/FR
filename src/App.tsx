f25ff9df8894b63625244f373ca754f19bd35a8f
stas <kepamuk@yandex.ru>
2025-12-19 12:37:12 +0300

    roting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.scss";
+import {Route, BrowserRouter, Routes, Navigate} from "react-router-dom";
+import {MainLayout} from "@app/layouts/MainLayout";
+import {APP_PATH} from "@shared/constants/constants.ts";
+import {HomePage} from "@pages/home";
+import {Bonuses} from "@pages/bonuses";
+import {Profile} from "@pages/profile";
+import {Invite} from "@pages/invite";
+import {Favorites} from "@pages/favorites";
 
 function App() {
-  return <>App</>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<MainLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path={APP_PATH.bonuses} element={<Bonuses/>}/>
+                    <Route path={APP_PATH.profile} element={<Profile/>}/>
+                    <Route path={APP_PATH.invite} element={<Invite/>}/>
+                    <Route path={APP_PATH.favorites} element={<Favorites/>}/>
+                    <Route path="*" element={<Navigate to={APP_PATH.main} replace />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>);
 }
 
 export default App;
