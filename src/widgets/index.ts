4af8e3c0c7d54313cfe3a65f43415fc6e72f82e0
stas <kepamuk@yandex.ru>
2025-12-24 18:34:36 +0300

    init

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -15,7 +15,7 @@ export { SearchModal } from './searchModal';
 export { SlideUpMenu } from './slideUpMenu';
 export { Preloader } from './preloader';
 export { TelegramLoginWidget } from './telegramLoginWidget';
-export { Footer } from './Footer';
 export { Carousel } from './carousel';
 export { MainCarouselSection } from './mainCarouselSection';
 export { HeroSection } from './HeroSection';
+export { Footer } from './footer/Footer';
